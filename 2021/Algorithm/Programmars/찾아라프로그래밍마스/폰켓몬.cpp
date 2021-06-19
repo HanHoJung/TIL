36a52c9b6c583373b6e48833245d49ad7fea2555
vitamindragon <gksghwjd0214@naver.com>
2021-06-20 01:04:04 +0900

    업데이트 소스

---
+++ 2021/Algorithm/Programmars/찾아라프로그래밍마스/폰켓몬.cpp
@@ -0,0 +1,15 @@
+/*
+https://earth-95.tistory.com/18
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout<<"Hello1";
+
+    int a =1;
+    int b = 2;
+
+    return 0;
+}
---
+++ 2021/Algorithm/test.cpp
@@ -0,0 +1,12 @@
+/*
+https://earth-95.tistory.com/18
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout<<"Hello";
+
+    return 0;
+}
